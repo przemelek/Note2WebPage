08c91ef8007b96286604acede4bd74179fc9bdfc
przemelek <przemelek@gmail.com>
2017-02-06 21:34:00 +0100

    fixes in formatting, popup should look slightly better

--- background.js
+++ background.js
@@ -1,45 +1,45 @@
-var state = {"domainComments":[],"pageComments":[]};
+var state = {"domainComments": [], "pageComments": []};
 
 function getDomain(url) {
-  if (url.indexOf("//")!=-1) {
-    url = url.substring(url.indexOf("//")+2);
-    if (url.indexOf("/")!=-1) {
-      url = url.substring(0,url.indexOf("/"));
+    if (url.indexOf("//") != -1) {
+        url = url.substring(url.indexOf("//") + 2);
+        if (url.indexOf("/") != -1) {
+            url = url.substring(0, url.indexOf("/"));
+        }
     }
-  }
-  return url;
+    return url;
 }
 
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-      if (request.cmd=="getComments") {
-        sendResponse(state);
-      }
+        if (request.cmd == "getComments") {
+            sendResponse(state);
+        }
     }
-  );
+);
 
 function updateBadge(tabId) {
-  chrome.tabs.get(tabId,function(tab) {
-    var url = tab.url;
-    var domain = getDomain(url);
-    var domainComments=JSON.parse(localStorage[domain]?localStorage[domain]:"[]");
-    var pageComments=JSON.parse(localStorage[url]?localStorage[url]:"[]");
-    state.domainComments=domainComments;
-    state.pageComments=pageComments;
-    var count = domainComments.length+pageComments.length;
-    var text=count>0?""+count:"";
-    chrome.browserAction.setBadgeText({"text":text});
-    chrome.browserAction.setBadgeBackgroundColor({"color":"red"});
-  });
+    chrome.tabs.get(tabId, function (tab) {
+        var url = tab.url;
+        var domain = getDomain(url);
+        var domainComments = JSON.parse(localStorage[domain] ? localStorage[domain] : "[]");
+        var pageComments = JSON.parse(localStorage[url] ? localStorage[url] : "[]");
+        state.domainComments = domainComments;
+        state.pageComments = pageComments;
+        var count = domainComments.length + pageComments.length;
+        var text = count > 0 ? "" + count : "";
+        chrome.browserAction.setBadgeText({"text": text});
+        chrome.browserAction.setBadgeBackgroundColor({"color": "red"});
+    });
 }
 
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-  updateBadge(activeInfo.tabId);
-})
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    updateBadge(activeInfo.tabId);
+});
 
-chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
-  if (tab.highlighted) {
-    updateBadge(tabId);
-  }
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.highlighted) {
+        updateBadge(tabId);
+    }
 });
--- popup.js
+++ popup.js
@@ -1,41 +1,41 @@
 function $(id) {
-  return document.getElementById(id);
+    return document.getElementById(id);
 }
 
 function getDomain(url) {
-  if (url.indexOf("//")!=-1) {
-    url = url.substring(url.indexOf("//")+2);
-    if (url.indexOf("/")!=-1) {
-      url = url.substring(0,url.indexOf("/"));
+    if (url.indexOf("//") != -1) {
+        url = url.substring(url.indexOf("//") + 2);
+        if (url.indexOf("/") != -1) {
+            url = url.substring(0, url.indexOf("/"));
+        }
     }
-  }
-  return url;
+    return url;
 }
 
 function clickHandler(wholePage) {
-  chrome.tabs.query( {active: true}, function(tab) {
-    var url = tab[0].url;
-    if (!wholePage) {
-      url = getDomain(url);
-    }
-    var fullPage=$("fullPage").checked;
-    var text = $("comment").value;
-    var comments=JSON.parse(localStorage[url]?localStorage[url]:"[]");
-    comments.push({"text":text,"fullPage":fullPage});
-    localStorage[url]=JSON.stringify(comments);
-    $("content").innerHTML=url;
-    $("fullPage").checked=false;
-    $("comment").value="";
-    refresh();
-  });
+    chrome.tabs.query({active: true}, function (tab) {
+        var url = tab[0].url;
+        if (!wholePage) {
+            url = getDomain(url);
+        }
+        var fullPage = $("fullPage").checked;
+        var text = $("comment").value;
+        var comments = JSON.parse(localStorage[url] ? localStorage[url] : "[]");
+        comments.push({"text": text, "fullPage": fullPage});
+        localStorage[url] = JSON.stringify(comments);
+        $("content").innerHTML = url;
+        $("fullPage").checked = false;
+        $("comment").value = "";
+        refresh();
+    });
 }
 
 function pageClickHandler() {
-  clickHandler(true);
+    clickHandler(true);
 }
 
 function domainClickHandler() {
-  clickHandler(false);
+    clickHandler(false);
 }
 
 function getCommentsFor(token) {
@@ -44,15 +44,15 @@ function getCommentsFor(token) {
 
 function refresh() {
     function convertToHTML(comments, scope) {
-        var content = ""
+        var content = "";
         for (var i = 0; i < comments.length; i++) {
             var idx = comments.length - i - 1;
-            content += comments[idx].text + "<button id=\""+scope+"_" + idx + "\">X</button>" + "<hr />";
+            content += "<tr><td>" + comments[idx].text + "</td><td valign='top'><button id=\"" + scope + "_" + idx + "\">x</button></td></tr>";
         }
         return content;
     }
 
-    function remove(idxToRemove,commentsArray) {
+    function remove(idxToRemove, commentsArray) {
         var comments = [];
         for (var j = 0; j < commentsArray.length; j++) {
             if (j != idxToRemove) {
@@ -62,36 +62,36 @@ function refresh() {
         return comments;
     }
 
-    chrome.tabs.query( {active: true}, function(tab) {
-    var url = tab[0].url;
-    var domain = getDomain(url);
-    var domainComments=getCommentsFor(domain);
-    var pageComments=getCommentsFor(url);
-    var content = convertToHTML(domainComments, "domain")+convertToHTML(pageComments,"page");
-    content=content.replace("\n","<br />");
-    $("content").innerHTML=content;
-    $("addToPage").onclick=pageClickHandler;
-    $("addToDomain").onclick=domainClickHandler;
-    var buttons = document.getElementsByTagName("button");
-    for (var i=0; i<buttons.length; i++) {
-      var button=buttons[i];
-      if (!button.onclick) {
-        button.onclick=function(element) {
-          var id=element.srcElement.id;
-          var elems=id.split("_");
-          if (elems[0]=="domain") {
-              localStorage[domain]=JSON.stringify(remove(elems[1]*1,domainComments));
-              refresh();
-          } else if (elems[0]=="page") {
-              localStorage[url]=JSON.stringify(remove(elems[1]*1,pageComments));
-              refresh();
-          }
-          }
+    chrome.tabs.query({active: true}, function (tab) {
+        var url = tab[0].url;
+        var domain = getDomain(url);
+        var domainComments = getCommentsFor(domain);
+        var pageComments = getCommentsFor(url);
+        var content = convertToHTML(domainComments, "domain") + convertToHTML(pageComments, "page");
+        content = "<table style='font-size:small;'>" + content.replace("\n", "<br />") + "</table>";
+        $("content").innerHTML = content;
+        $("addToPage").onclick = pageClickHandler;
+        $("addToDomain").onclick = domainClickHandler;
+        var buttons = document.getElementsByTagName("button");
+        for (var i = 0; i < buttons.length; i++) {
+            var button = buttons[i];
+            if (!button.onclick) {
+                button.onclick = function (element) {
+                    var id = element.srcElement.id;
+                    var elems = id.split("_");
+                    if (elems[0] == "domain") {
+                        localStorage[domain] = JSON.stringify(remove(elems[1] * 1, domainComments));
+                        refresh();
+                    } else if (elems[0] == "page") {
+                        localStorage[url] = JSON.stringify(remove(elems[1] * 1, pageComments));
+                        refresh();
+                    }
+                }
+            }
         }
-    }
-  });
+    });
 }
-window.onload=function() {
-  refresh();
+window.onload = function () {
+    refresh();
 };
 
